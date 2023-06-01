0591d5cb7bf64fe5bd603d3188700d2c371442f6
Void00000000 <chernikovearl@gmail.com>
2023-06-02 00:40:08 +0700

    changed structure of the file system of project

---
+++ MakeGrid3D/Common.cs
@@ -0,0 +1,175 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeGrid3D
+{
+    enum NodeType : byte
+    {
+        Regular,
+        Left,
+        Right,
+        Top,
+        Bottom,
+        Removed
+    }
+    enum Direction
+    {
+        Left,
+        Right,
+        Top,
+        Bottom,
+    }
+
+    enum Quadrant
+    {
+        RightTop,
+        LeftTop,
+        LeftBottom,
+        RightBottom
+    }
+
+    enum Plane
+    {
+        XY,
+        XZ,
+        YZ,
+    }
+    interface IGrid
+    {
+        public int Nnodes { get; }
+        public int Nelems { get; }
+        public int Nmats { get; }
+        public float MeanAR { get; set; }
+        public float WorstAR { get; set; }
+    }
+
+    class GridState
+    {
+        public IGrid Grid { get; }
+        public int I { get; } = 1;
+        public int J { get; } = 1;
+        public int K { get; } = 1;
+        public int MidI { get; } = 1;
+        public int MidJ { get; } = 1;
+        public int MidK { get; } = 1;
+        public int NodeI { get; } = 1;
+        public int NodeJ { get; } = 1;
+        public int NodeK { get; } = 1;
+        public int QuadIndex { get; } = 0;
+        public int DirIndex { get; } = 0;
+        public bool End { get; } = false;
+        public GridState(IGrid grid, IrregularGridMaker irregularGridMaker)
+        {
+            Grid = grid;
+            I = irregularGridMaker.I;
+            J = irregularGridMaker.J;
+            K = irregularGridMaker.K;
+            MidI = irregularGridMaker.MidI;
+            MidJ = irregularGridMaker.MidJ;
+            MidK = irregularGridMaker.MidK;
+            NodeI = irregularGridMaker.NodeI;
+            NodeJ = irregularGridMaker.NodeJ;
+            NodeK = irregularGridMaker.NodeK;
+            QuadIndex = irregularGridMaker.QuadIndex;
+            DirIndex = irregularGridMaker.DirIndex;
+            End = irregularGridMaker.End;
+        }
+        public GridState(IGrid grid)
+        {
+            Grid = grid;
+        }
+    }
+
+    // TODO: В UI значения по умолчаниюплохо ставятся при загрузке новой сетки
+    class Mesh
+    {
+        public int Vao { get; private set; }
+        public int Vbo { get; private set; }
+        public int Ebo { get; private set; }
+        public int VLen { get; private set; }
+        public int ILen { get; private set; }
+        public Mesh(int vbo, uint[] indices, int vLen)
+        {
+            Vbo = vbo;
+            Vao = GL.GenVertexArray();
+            GL.BindVertexArray(Vao);
+            Ebo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, Ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+            VLen = vLen;
+            ILen = indices.Length;
+            GL.BindBuffer(BufferTarget.ArrayBuffer, Vbo);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            GL.BindVertexArray(0);
+        }
+
+        public Mesh(float[] vertices, uint[] indices)
+        {
+            Vbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, Vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            Vao = GL.GenVertexArray();
+            GL.BindVertexArray(Vao);
+            Ebo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, Ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+            VLen = vertices.Length;
+            ILen = indices.Length;
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            GL.BindVertexArray(0);
+        }
+
+        public Mesh(Mesh mesh)
+        {
+            Vao = mesh.Vao;
+            Vbo = mesh.Vbo;
+            Ebo = mesh.Ebo;
+            VLen = mesh.VLen;
+            ILen = mesh.ILen;
+        }
+
+        public void Use()
+        {
+            GL.BindVertexArray(Vao);
+        }
+
+        public void DrawElems(int count, int offset, PrimitiveType type)
+        {
+            GL.BindVertexArray(Vao);
+            GL.DrawElements(type, count, DrawElementsType.UnsignedInt, offset * sizeof(uint));
+        }
+
+        public void DrawElems(PrimitiveType type)
+        {
+            GL.BindVertexArray(Vao);
+            GL.DrawElements(type, ILen, DrawElementsType.UnsignedInt, 0);
+        }
+
+        public void DrawVerices(int count, int offset, PrimitiveType type)
+        {
+            GL.BindVertexArray(Vao);
+            GL.DrawArrays(type, offset, count);
+        }
+
+        public void DrawVerices(PrimitiveType type)
+        {
+            GL.BindVertexArray(Vao);
+            GL.DrawArrays(type, 0, VLen);
+        }
+
+        public void Dispose()
+        {
+            GL.DeleteBuffer(Vbo);
+            GL.DeleteVertexArray(Vao);
+            GL.DeleteBuffer(Ebo);
+        }
+    }
+}
---
+++ MakeGrid3D/CrossSections.cs
@@ -0,0 +1,153 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeGrid3D
+{
+    // TODO: в нерегулярных сетках не все слои выделяются
+    class CrossSections
+    {
+        // XY - 0; XZ - 1; YZ - 2;
+        private List<Tuple<Mesh, float>>[] planes = new List<Tuple<Mesh, float>>[3];
+        private int indexPlane = 0;
+        private int indexPlaneSec = -1;
+        public int MaxPlaneIndex { get; private set; } = int.MinValue;
+        public bool Active { get; set; } = false;
+        public float CurrentValue { get; private set; }
+        public Plane CurrentPlane
+        {
+            get { return (Plane)indexPlane; }
+            set
+            {
+                try
+                {
+                    indexPlane = (int)value;
+                    MaxPlaneIndex = planes[indexPlane].Count - 1;
+                    indexPlaneSec = -1;
+                    Active = false;
+                }
+                catch (InvalidCastException) { Active = false; }
+            }
+        }
+
+        public int CurrentPlaneSec
+        {
+            get => indexPlaneSec;
+            set
+            {
+                if (value < 0)
+                {
+                    indexPlaneSec = -1;
+                    Active = false;
+                }
+                else if (value > MaxPlaneIndex)
+                {
+                    indexPlaneSec = MaxPlaneIndex + 1;
+                    Active = false;
+                }
+                else
+                {
+                    Active = true;
+                    indexPlaneSec = value;
+                    CurrentValue = planes[indexPlane][indexPlaneSec].Item2;
+                }
+            }
+        }
+
+        public Color4 PlaneColor { get; set; } = new Color4(50 / 255f, 168 / 255f, 107 / 255f, 0.75f);
+        public CrossSections(Grid3D grid3D)
+        {
+            float xmin = grid3D.Area.X0;
+            float ymin = grid3D.Area.Y0;
+            float zmin = grid3D.Area.Z0;
+            float xmax = grid3D.Area.Xn;
+            float ymax = grid3D.Area.Yn;
+            float zmax = grid3D.Area.Zn;
+            // XY
+            planes[0] = new List<Tuple<Mesh, float>>(grid3D.Nz);
+            for (int k = 0; k < grid3D.Nz; k++)
+            {
+                int n = -1;
+                for (int j = 0; j < grid3D.Ny; j++)
+                    for (int i = 0; i < grid3D.Nx; i++)
+                    {
+                        n = grid3D.global_num(i, j, k);
+                        if (n >= 0)
+                            break;
+                    }
+                if (n >= 0)
+                {
+                    float z = grid3D.XYZ[n].Z;
+                    float[] vertices = { xmin, ymin, z,
+                                         xmax, ymin, z,
+                                         xmin, ymax, z,
+                                         xmax, ymax, z};
+                    uint[] indices = { 0, 2, 3, 0, 1, 3 };
+                    Mesh mesh = new Mesh(vertices, indices);
+                    planes[0].Add(Tuple.Create(mesh, z));
+                }
+            }
+            // XZ
+            planes[1] = new List<Tuple<Mesh, float>>(grid3D.Ny);
+            for (int j = 0; j < grid3D.Ny; j++)
+            {
+                int n = -1;
+                for (int k = 0; k < grid3D.Nz; k++)
+                    for (int i = 0; i < grid3D.Nx; i++)
+                    {
+                        n = grid3D.global_num(i, j, k);
+                        if (n >= 0)
+                            break;
+                    }
+                if (n >= 0)
+                {
+                    float y = grid3D.XYZ[n].Y;
+                    float[] vertices = { xmin, y, zmin,
+                                         xmax, y, zmin,
+                                         xmin, y, zmax,
+                                         xmax, y, zmax};
+                    uint[] indices = { 0, 2, 3, 0, 1, 3 };
+                    Mesh mesh = new Mesh(vertices, indices);
+                    planes[1].Add(Tuple.Create(mesh, y));
+                }
+            }
+            // YZ
+            planes[2] = new List<Tuple<Mesh, float>>(grid3D.Nx);
+            for (int i = 0; i < grid3D.Nx; i++)
+            {
+                int n = -1;
+                for (int k = 0; k < grid3D.Nz; k++)
+                    for (int j = 0; j < grid3D.Ny; j++)
+                    {
+                        n = grid3D.global_num(i, j, k);
+                        if (n >= 0)
+                            break;
+                    }
+                if (n >= 0)
+                {
+                    float x = grid3D.XYZ[n].X;
+                    float[] vertices = { x, ymin, zmin,
+                                     x, ymax, zmin,
+                                     x, ymin, zmax,
+                                     x, ymax, zmax};
+                    uint[] indices = { 0, 2, 3, 0, 1, 3 };
+                    Mesh mesh = new Mesh(vertices, indices);
+                    planes[2].Add(Tuple.Create(mesh, x));
+                }
+            }
+        }
+
+        public void DrawPlane(Shader shader)
+        {
+            if (Active)
+            {
+                shader.SetColor4("current_color", PlaneColor);
+                planes[indexPlane][indexPlaneSec].Item1.DrawElems(6, 0, PrimitiveType.Triangles);
+            }
+        }
+    }
+}
